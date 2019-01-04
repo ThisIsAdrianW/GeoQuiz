3da7708d3b5c2c096deb505fd84fa326f2e53dd2
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2019-01-04 16:22:41 +0100

    Added id`s for buttons Fixed string resources (no more app name there)

--- app/src/main/java/com/thisis/adrianw/geoquiz/QuizActivity.java
+++ app/src/main/java/com/thisis/adrianw/geoquiz/QuizActivity.java
@@ -3,8 +3,11 @@ package com.thisis.adrianw.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
